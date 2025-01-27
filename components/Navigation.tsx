b011c1e739877546b714d7eb55c0843f580b6aab
Mesfin Girma <mesfingirmatwo@gmail.com>
2025-01-27 08:35:23 +0300

    added theme toggler on nav items

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ModeToggle } from './theme-toggler'
 
 const Navigation = () => {
   return (
@@ -16,6 +17,7 @@ const Navigation = () => {
              <li> <a href="">Work</a></li>
               <li><a href="">Testimonials</a></li>
           </ul>
+          <ModeToggle/>
         </div>
    
   )
