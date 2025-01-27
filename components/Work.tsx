c00be7de4be1ccf62d22ae16065ff6cb8676f2d5
Mesfin Girma <mesfingirmatwo@gmail.com>
2025-01-27 09:25:30 +0300

    created constants for work experience

--- components/Work.tsx
+++ components/Work.tsx
@@ -4,6 +4,12 @@ const Work = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
       <h1 className='text-4xl'>Experience</h1>
+
+      <div>
+        <div>
+            <span></span>
+        </div>
+      </div>
     </div>
   )
 }
