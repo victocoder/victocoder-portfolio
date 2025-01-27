8cfd6b959acc5684e07b6a52205e08e33f34f59e
Mesfin Girma <mesfingirmatwo@gmail.com>
2025-01-27 08:55:25 +0300

    added a basic download cv button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex justify-center items-center pt-10 flex-col'>
+    <div className='flex justify-center items-center pt-[150px] flex-col gap-4 px-4'>
         <h1 className='text-6xl md:text-8xl'>Mesfin Girma</h1>
       <p>I am Software Developer</p>
-      <p>A professional in solving tech related problems and helping companies and individuals create high quality software appications</p>
+      <p className='text-center'>A professional in solving tech related problems and helping companies and individuals own high quality software appications</p>
+
+      <button className='border border-red-100 px-2 py-2 rounded-sm'>Download Cv</button>
     </div>
   )
 }
